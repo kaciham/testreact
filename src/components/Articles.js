d379af4119ef03d1cd6912273adabfd2ac6d0995
Kaci Hamroun <kacihamrounpro@gmail.com>
2023-07-13 14:09:24 +0200

    Merge pull request #1 from kaciham/test
    Test

---
+++ src/components/Articles.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Articles = ({ article }) => {
+  const dateFormater = (date) => {
+    let newDate = new Date(date).toLocaleDateString("fr-FR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    });
+
+    return newDate;
+  };
+
+  return (
+    <div className="article">
+      <div className="card-header">
+        <h3>{article.author}</h3>
+        <em>Posté le {dateFormater(article.date)}</em>
+      </div>
+      <p>{article.content}</p>
+      <div className="btn-container">
+        <button>Edit</button>
+        <button>Supprimer</button>
+      </div>
+    </div>
+  );
+};
+
+export default Articles;
